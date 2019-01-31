90c649de9e683d264495affee8b3cbd3de175162
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2019-01-31 16:31:50 -0600

    project created

---
+++ app/src/main/java/comp3350/schrodingers/presentation/HomeActivity.java
@@ -0,0 +1,15 @@
+package comp3350.schrodingers.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import comp3350.schrodingers.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
