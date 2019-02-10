41b2f8cc05f89c43ea2161f5d648075bffb98eaa
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2019-02-09 20:29:14 -0600

    user info update

--- app/src/main/java/comp3350/schrodingers/presentation/PersonInfo.java
+++ app/src/main/java/comp3350/schrodingers/presentation/PersonInfo.java
@@ -3,7 +3,12 @@ package comp3350.schrodingers.presentation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import comp3350.schrodingers.persistence.stubs.UsersPersistenceStub;
 import comp3350.schrodingers.Objects.User;
 
@@ -28,5 +33,38 @@ public class PersonInfo extends AppCompatActivity {
         //userAddress.setText(User.getAddress());
         EditText userPhone = (EditText) findViewById(R.id.phone);
         //userPhone.setText(User.getPhone());
+    }
+
+    public void buttonInfoUpdate(View v){
+        EditText editName = (EditText)findViewById(R.id.username);
+        EditText editEmail = (EditText)findViewById(R.id.email);
+        EditText editAddress = (EditText)findViewById(R.id.address);
+        EditText editPhone = (EditText)findViewById(R.id.phone);
+
+        String validate = validateInfo(editName.getText().toString(),editEmail.getText().toString());
+        if(validate == null){
+            //user.changeName(editName.getText().toString());
+            //user.changeEmail(editEmail.getText().toString());
+            System.out.println();
+        }else{
+            Messages.warning(this, validate);
+        }
+
+
+    }
+    private String validateInfo(String name, String email){
+        if(name.length() == 0)
+            return "Name required";
+
+        if(email.length() == 0)
+            return "Email required";
+        else{
+            String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(email);
+            if(!matcher.matches())
+                return "Email not valid";
+        }
+        return null;
     }
 }
