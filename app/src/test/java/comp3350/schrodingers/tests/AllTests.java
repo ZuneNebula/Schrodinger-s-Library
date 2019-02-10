aab7a140ddbfc23cd86ecaef2eabff74ee31765d
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2019-02-10 00:20:27 -0600

    some files added

---
+++ app/src/test/java/comp3350/schrodingers/tests/AllTests.java
@@ -0,0 +1,15 @@
+package comp3350.schrodingers.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import comp3350.schrodingers.tests.objects.BillingTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        BillingTest.class
+})
+public class AllTests
+{
+
+}
