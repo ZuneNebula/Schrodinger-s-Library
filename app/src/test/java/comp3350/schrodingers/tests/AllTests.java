892fd95e448b970c5b6e0b0853a8ad5528b15139
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2019-02-11 13:46:55 -0600

    tests

--- app/src/test/java/comp3350/schrodingers/tests/AllTests.java
+++ app/src/test/java/comp3350/schrodingers/tests/AllTests.java
@@ -1,18 +1,15 @@
 package comp3350.schrodingers.tests;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import comp3350.schrodingers.tests.business.PaymentProcessorTest;
 import comp3350.schrodingers.tests.objects.BillingTest;
-import comp3350.schrodingers.tests.business.AccessPaymentInfoTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         BillingTest.class,
-        AccessPaymentInfoTest.class
+        PaymentProcessorTest.class
 })
 public class AllTests
 {
