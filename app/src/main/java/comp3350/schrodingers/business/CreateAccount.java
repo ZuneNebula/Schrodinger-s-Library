d4f0eae19bb32e5f8ab7b555f84f5c5ca4bc4f4c
Mihir Sharma <sharma48@myumanitoba.ca>
2019-02-10 20:29:10 -0600

    Upload New File

---
+++ app/src/main/java/comp3350/schrodingers/business/CreateAccount.java
@@ -0,0 +1,29 @@
+package comp3350.schrodingers.business;
+
+import java.util.Collections;
+import java.util.List;
+
+import comp3350.schrodingers.Objects.User;
+import comp3350.schrodingers.application.Services;
+import comp3350.schrodingers.persistence.UsersPersistence;
+
+public class CreateAccount {
+    private UsersPersistence usersPersistence;
+
+    public CreateAccount() {
+
+        //constructor
+        usersPersistence = Services.getUsersPersistence();
+
+    }
+
+    public User insertUser(String email, String userName, String password) {
+        User newUser = new User(email, userName, password);
+        return usersPersistence.insertUser(newUser);
+
+    }
+
+    public void deleteUser(String email) {
+        usersPersistence.deleteUser(email);
+    }
+}
