15ae1f66a622728f0d5e3fa2e34b8aff31a80d6e
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2019-02-10 20:09:39 -0600

    paymentTest

---
+++ app/src/test/java/comp3350/schrodingers/tests/business/AccessPaymentInfoTest.java
@@ -0,0 +1,13 @@
+package comp3350.schrodingers.tests.business;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import comp3350.schrodingers.business.AccessPaymentInfo;
+
+public class AccessPaymentInfoTest {
+    @Test
+    public void testPaymentInfo(){
+        System.out.println("\nStarting testBilling");
+
+        System.out.println("Finished testBilling");
+    }
+}
