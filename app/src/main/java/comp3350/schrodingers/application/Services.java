568f7c3a82fcd27ec7fc1fffcdb1eafa3e14b995
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2019-02-21 15:24:42 -0600

    services

--- app/src/main/java/comp3350/schrodingers/application/Services.java
+++ app/src/main/java/comp3350/schrodingers/application/Services.java
@@ -1,11 +1,11 @@
 package comp3350.schrodingers.application;
 
 import comp3350.schrodingers.persistence.BooksPersistence;
-import comp3350.schrodingers.persistence.stubs.BooksPersistenceStub;
+import comp3350.schrodingers.persistence.hsqldb.BooksPersistenceHSQLDB;
 import comp3350.schrodingers.persistence.UsersPersistence;
-import comp3350.schrodingers.persistence.stubs.UsersPersistenceStub;
+import comp3350.schrodingers.persistence.hsqldb.UsersPersistenceHSQLDB;
 import comp3350.schrodingers.persistence.PaymentPersistence;
-import comp3350.schrodingers.persistence.stubs.PaymentPersistenceStub;
+import comp3350.schrodingers.persistence.hsqldb.PaymentPersistenceHSQLDB;
 
 public class Services {
 
@@ -15,21 +15,21 @@ public class Services {
 
     public static synchronized BooksPersistence getBooksPersistence(){
         if (booksPersistence == null){
-            booksPersistence= new BooksPersistenceStub();
+            booksPersistence= new BooksPersistenceHSQLDB();
         }
         return booksPersistence;
     }
 
     public static synchronized UsersPersistence getUsersPersistence(){
         if (usersPersistence == null){
-            usersPersistence= new UsersPersistenceStub();
+            usersPersistence= new UsersPersistenceHSQLDB();
         }
         return usersPersistence;
     }
 
     public static synchronized PaymentPersistence getPaymentPersistence(){
         if (paymentPersistence == null){
-            paymentPersistence = new PaymentPersistenceStub();
+            paymentPersistence = new PaymentPersistenceHSQLDB();
         }
         return paymentPersistence;
     }
