9366fc96323279439387faf63f16ad48520e7a28
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2019-02-20 21:32:45 -0600

    PaymentProcessorTest

--- app/src/test/java/comp3350/schrodingers/tests/business/PaymentProcessorTest.java
+++ app/src/test/java/comp3350/schrodingers/tests/business/PaymentProcessorTest.java
@@ -3,100 +3,112 @@ package comp3350.schrodingers.tests.business;
 import junit.framework.TestCase;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import comp3350.schrodingers.business.CardException;
 import comp3350.schrodingers.business.PaymentProcessor;
+import comp3350.schrodingers.objects.User.Billing;
 
 public class PaymentProcessorTest extends TestCase {
     private PaymentProcessor validator;
-    private String messageValid;
-    private String[] correctVal = {"1234123412341234","11/22","123","JOHN SMITH"};
+    private boolean messageValid;
+    private long correctNum = 1234123412341234L;
+    private int correctCvv = 123;
+    private String correctDate = "11/22";
+    private String correctName = "JOHN SMITH";
+    private Billing card;
 
     public PaymentProcessorTest(String arg0){
         super(arg0);
         validator = new PaymentProcessor();
+        card=null;
+    }
+    private void cardHandler(Billing card){
+        try {
+            validator.validateCard(card);
+        }catch(CardException c){
+            System.out.println("\t"+c);
+        }
     }
 
-    @Test
+    @Test(expected= CardException.class)
     public void testEmptyCardNumber(){
         System.out.println("\nStarting testPaymentProcessor: empty Card Number");
-        messageValid = validator.validateCard("",correctVal[1],correctVal[2],correctVal[3]);
-        System.out.println("\n\t"+messageValid+"\n");
+        card = new Billing(0L,correctName,correctDate,correctCvv);
+        cardHandler(card);
         System.out.println("\nFinished testPaymentProcessor: empty Card Number");
     }
-    @Test
+    @Test(expected= CardException.class)
     public void testShortCardNumber(){
         System.out.println("\nStarting testPaymentProcessor: long Card Number");
-        String longNumber = "999999999";
-        System.out.println("\tLong card size "+longNumber.length()+":");
-        messageValid = validator.validateCard(longNumber,correctVal[1],correctVal[2],correctVal[3]);
-        System.out.println("\n\t"+messageValid);
+        card = new Billing(999999999L,correctName,correctDate,correctCvv);
+        cardHandler(card);
         System.out.println("\nFinished testPaymentProcessor: long Card Number");
     }
-    @Test
+    @Test(expected= CardException.class)
     public void testEmptyDate(){
         System.out.println("\nStarting testPaymentProcessor: empty Date");
-        messageValid = validator.validateCard(correctVal[0],"",correctVal[2],correctVal[3]);
-        System.out.println("\n\t"+messageValid+"\n");
+        card = new Billing(999999999L,"",correctDate,correctCvv);
+        cardHandler(card);
         System.out.println("\nFinished testPaymentProcessor: empty Date");
     }
-    @Test
+    @Test(expected= CardException.class)
     public void testIncorrectFormatDate(){
         System.out.println("\nStarting testPaymentProcessor: incorrect Date format");
         String incDate = "012019";
         System.out.println("\t"+incDate+":");
-        messageValid = validator.validateCard(correctVal[0],incDate,correctVal[2],correctVal[3]);
-        System.out.println("\n\t"+messageValid);
+        card = new Billing(correctNum,correctName,incDate,correctCvv);
+        cardHandler(card);
         incDate = "08/2019";
         System.out.println("\t"+incDate+":");
-        messageValid = validator.validateCard(correctVal[0],incDate,correctVal[2],correctVal[3]);
-        System.out.println("\n\t"+messageValid);
+        card = new Billing(correctNum,correctName,incDate,correctCvv);
+        cardHandler(card);
         incDate = "5/19";
         System.out.println("\t"+incDate+":");
-        messageValid = validator.validateCard(correctVal[0],incDate,correctVal[2],correctVal[3]);
-        System.out.println("\n\t"+messageValid);
+        card = new Billing(correctNum,correctName,incDate,correctCvv);
+        cardHandler(card);
         System.out.println("\nFinished testPaymentProcessor: incorrect Date format");
     }
-    @Test
+    @Test(expected= CardException.class)
     public void testExpiredDate(){
         System.out.println("\nStarting testPaymentProcessor: expired Date");
         System.out.println("\tSame year, past month:");
         String incDate = "01/19";
-        messageValid = validator.validateCard(correctVal[0],incDate,correctVal[2],correctVal[3]);
-        System.out.println("\n\t"+messageValid);
+        card = new Billing(correctNum,correctName,incDate,correctCvv);
+        cardHandler(card);
         System.out.println("\tPast year:");
         incDate = "01/16";
-        messageValid = validator.validateCard(correctVal[0],incDate,correctVal[2],correctVal[3]);
-        System.out.println("\n\t"+messageValid);
+        card = new Billing(correctNum,correctName,incDate,correctCvv);
+        cardHandler(card);
         System.out.println("\nFinished testPaymentProcessor: expired Date");
     }
-    @Test
+    @Test(expected= CardException.class)
     public void testSuspiciousDate(){
         System.out.println("\nStarting testPaymentProcessor: suspicious Date");
         String incDate = "01/68";
-        messageValid = validator.validateCard(correctVal[0],incDate,correctVal[2],correctVal[3]);
-        System.out.println("\n\t"+messageValid);
+        card = new Billing(correctNum,correctName,incDate,correctCvv);
+        cardHandler(card);
         System.out.println("\nFinished testPaymentProcessor: suspicious Date");
     }
-    @Test
+    @Test(expected= CardException.class)
     public void testEmptyCvv(){
         System.out.println("\nStarting testPaymentProcessor: empty CVV");
-        messageValid = validator.validateCard(correctVal[0],correctVal[1],"",correctVal[3]);
-        System.out.println("\n\t"+messageValid);
+        card = new Billing(correctNum,correctName,correctDate,0);
+        cardHandler(card);
         System.out.println("\nFinished testPaymentProcessor: empty CVV");
     }
-    @Test
+    @Test(expected= CardException.class)
     public void testShortCvv(){
         System.out.println("\nStarting testPaymentProcessor: short CVV");
-        String shortCvv = "1";
-        messageValid = validator.validateCard(correctVal[0],correctVal[1],shortCvv,correctVal[3]);
-        System.out.println("\n\t"+messageValid);
+        card = new Billing(correctNum,correctName,correctDate,1);
+        cardHandler(card);
         System.out.println("\nFinished testPaymentProcessor: short CVV");
     }
-    @Test
+    @Test(expected= CardException.class)
     public void testEmptyName(){
         System.out.println("\nStarting testPaymentProcessor: empty name");
-        messageValid = validator.validateCard(correctVal[0],correctVal[1],correctVal[2],"");
-        System.out.println("\n\t"+messageValid+"\n");
+        card = new Billing(correctNum,"",correctDate,correctCvv);
+        cardHandler(card);
         System.out.println("\nFinished testPaymentProcessor: empty name");
     }
 }
