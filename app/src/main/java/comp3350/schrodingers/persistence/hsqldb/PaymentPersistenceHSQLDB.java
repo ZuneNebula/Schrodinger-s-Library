6db65ff96904cc2cde19b6a2eace6a30c13fcc27
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2019-02-21 20:33:12 -0600

    paymentPersistanceHSQLDB init

--- app/src/main/java/comp3350/schrodingers/persistence/hsqldb/PaymentPersistenceHSQLDB.java
+++ app/src/main/java/comp3350/schrodingers/persistence/hsqldb/PaymentPersistenceHSQLDB.java
@@ -1,22 +1,84 @@
 package comp3350.schrodingers.persistence.hsqldb;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import comp3350.schrodingers.objects.User.Billing;
 import comp3350.schrodingers.persistence.PaymentPersistence;
 
 public class PaymentPersistenceHSQLDB implements PaymentPersistence {
+    private final Connection c;
+    public PaymentPersistenceHSQLDB(final String dbPath){
+        try {
+            this.c = DriverManager.getConnection("jdbc:hsqldb:file:" + dbPath, "SA", "");
+        } catch (final SQLException e) {
+            throw new PersistenceException(e);
+        }
+    }
+    private Billing fromResultSet(final ResultSet rs) throws SQLException {
+        final long cardNum = rs.getLong("cardNum");
+        final String cardName = rs.getString("cardName");
+        final String expiryDate = rs.getString("expiryDate");
+        final int cvv = rs.getInt("cvv");
+        return new Billing(cardNum,cardName,expiryDate,cvv);
+    }
     public Billing addCreditCard(Billing creditCard){
-        return null;
+        try {
+            final PreparedStatement st = c.prepareStatement("INSERT INTO creditCard VALUES(?, ?, ?, ?)");
+            st.setLong(1, creditCard.getCardNumber());
+            st.setString(2, creditCard.getFullName());
+            st.setString(3, creditCard.getExpiry());
+            st.setInt(4, creditCard.getCvv());
+
+            st.executeUpdate();
+
+            return creditCard;
+        } catch (final SQLException e) {
+            throw new PersistenceException(e);
+        }
     }
     public Billing updateCreditCard(Billing creditCard){
-        return null;
+        try {
+            final PreparedStatement st = c.prepareStatement("UPDATE creditCard SET name = ? WHERE cardNum = ?");
+            st.setLong(1, creditCard.getCardNumber());
+            st.setString(2, creditCard.getFullName());
+            st.setString(3, creditCard.getExpiry());
+            st.setInt(4, creditCard.getCvv());
+
+            st.executeUpdate();
+
+            return creditCard;
+        } catch (final SQLException e) {
+            throw new PersistenceException(e);
+        }
     }
     public void deleteCreditCard(Billing creditCard){
 
     }
 
     public List<Billing> getCards(){
-        return null;
+        final List<Billing> cards = new ArrayList<>();
+
+        try{
+            final Statement st = c.createStatement();
+            final ResultSet rs = st.executeQuery("SELECT * FROM courses");
+            while (rs.next()){
+                final Billing card = fromResultSet(rs);
+                cards.add(card);
+            }
+            rs.close();
+            st.close();
+
+            return cards;
+        }
+        catch (final SQLException e){
+            throw new PersistenceException(e);
+        }
     }
 }
