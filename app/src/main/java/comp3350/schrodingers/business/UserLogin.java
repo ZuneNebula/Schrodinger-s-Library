86edfeb431a8235687fa6cf54ecf06c70d69355a
Mihir Sharma <sharma48@myumanitoba.ca>
2019-02-25 03:23:58 -0600

    Upload New File

---
+++ app/src/main/java/comp3350/schrodingers/business/UserLogin.java
@@ -0,0 +1,35 @@
+package comp3350.schrodingers.business;
+
+import java.util.Collections;
+import java.util.List;
+
+import comp3350.schrodingers.objects.User;
+import comp3350.schrodingers.application.Services;
+import comp3350.schrodingers.persistence.UsersPersistence;
+
+public class UserLogin {
+    private UsersPersistence usersPersistence;
+
+    public UserLogin()
+    {
+        //constructor
+        usersPersistence = Services.getUsersPersistence();
+
+    }
+
+    public User checkLogin(String email, String password) {
+        User curr = usersPersistence.findUser(email);
+        if (curr!=null && curr.getPassword()==password)
+        {
+            return curr;
+        }
+        else
+        {
+            //System.out.println("failed login");
+            return null;
+        }
+
+    }
+
+}
+
