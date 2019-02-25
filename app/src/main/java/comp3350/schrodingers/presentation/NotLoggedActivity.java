998c9c040ef02543e6bc87b1b1cd9bc09ddc0c63
Mihir Sharma <sharma48@myumanitoba.ca>
2019-02-25 03:25:09 -0600

    Upload New File

---
+++ app/src/main/java/comp3350/schrodingers/presentation/NotLoggedActivity.java
@@ -0,0 +1,49 @@
+package comp3350.schrodingers.presentation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+import comp3350.schrodingers.persistence.stubs.UsersPersistenceStub;
+import comp3350.schrodingers.objects.User;
+
+import comp3350.schrodingers.R;
+
+public class NotLoggedActivity extends AppCompatActivity {
+
+    private Button Login;
+    private Button Account;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_not_logged);
+
+        Login = (Button)findViewById(R.id.login_button);
+        Account = (Button) findViewById(R.id.account_button);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NotLoggedActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        Account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NotLoggedActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+}
