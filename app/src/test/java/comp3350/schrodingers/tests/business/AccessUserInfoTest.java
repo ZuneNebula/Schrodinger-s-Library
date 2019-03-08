cf564ce1502bc6fbb4bbe1f07be9d1bd1f3c82b5
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2019-03-08 01:04:51 -0600

    Update AccessUserInfoTest.java

--- app/src/test/java/comp3350/schrodingers/tests/business/AccessUserInfoTest.java
+++ app/src/test/java/comp3350/schrodingers/tests/business/AccessUserInfoTest.java
@@ -8,6 +8,7 @@ import comp3350.schrodingers.objects.User;
 import comp3350.schrodingers.tests.persistence.UsersPersistenceStub;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 public class AccessUserInfoTest {
     private AccessUserInfo accessUserInfo;
@@ -25,6 +26,25 @@ public class AccessUserInfoTest {
         assertNotNull("\tfirst logged user should not be null", user);
         assertTrue("Zune".equals(user.getUserName()));
         System.out.println("\nFinished test AccessUserInfo");
+    }
+    @Test
+    public void testLogout(){
+        final User user;
+        System.out.println("\nStarting test Logout");
+        accessUserInfo.logout();
+        user = accessUserInfo.getUser();
+        assertNull("\tuser should be null", user);
+        System.out.println("\nFinished test Logout");
+    }
+
+    @Test
+    public void testLogin(){
+        final User user;
+        System.out.println("\nStarting test Login");
+        user = accessUserInfo.login("zunenebula@gmail.com");
+        assertNotNull("\tuser should not be null", user);
+        assertTrue("Zune".equals(user.getUserName()));
+        System.out.println("\nFinished test Login");
     }
 
 }
