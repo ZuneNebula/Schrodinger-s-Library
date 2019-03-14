f9a1932a7f596179beed038e7b154e1fcaac2b2e
Richard Constantine <constan7@myumanitoba.ca>
2019-03-14 18:45:35 -0500

    Updated comments and general tidying.

--- app/src/main/java/comp3350/schrodingers/presentation/PurchaseBookActivity.java
+++ app/src/main/java/comp3350/schrodingers/presentation/PurchaseBookActivity.java
@@ -22,15 +22,15 @@ public class PurchaseBookActivity extends AppCompatActivity {
     String email = currUser.getEmail();
 
     // Get address info
-    boolean isEmpty = userAddress.isEmpty();
-    if (!isEmpty)
-    {
-        String address = userAddress.getAddress();
-        String postalCode = userAddress.getPostalCode();
-        String country = userAddress.getCountry();
-        String state = userAddress.getState();
-        String city = userAddress.getCity();
-    }
+//    boolean isEmpty = userAddress.isEmpty();
+//    if (!isEmpty)
+//    {
+//        String address = userAddress.getAddress();
+//        String postalCode = userAddress.getPostalCode();
+//        String country = userAddress.getCountry();
+//        String state = userAddress.getState();
+//        String city = userAddress.getCity();
+//    }
 
     // Get bill info
     long creditCard = userBilling.getCardNumber();
@@ -56,35 +56,35 @@ public class PurchaseBookActivity extends AppCompatActivity {
 
     private void displayInfo(){
 
-        TextView review_heading = findViewById(R.id.review_heading);
-        review_heading.setText("Review Your Order");
-
-        TextView review_userName = findViewById(R.id.review_userName);
-        review_userName.setText(userName);
-
-        TextView review_email = findViewById(R.id.review_email);
-        review_email.setText(email);
-
-        TextView review_address = findViewById(R.id.review_address);
-        review_address.setText(address);
-
-        TextView review_postalCode = findViewById(R.id.review_postalCode);
-        review_postalCode.setText(postalCode);
-
-        TextView review_country = findViewById(R.id.review_country);
-        review_country.setText(country);
-
-        TextView review_state = findViewById(R.id.review_state);
-        review_state.setText(state);
-
-        TextView review_city = findViewById(R.id.review_city);
-        review_city.setText(city);
-
-        TextView review_cardNo = findViewById(R.id.review_creditCard);
-        review_cardNo.setText(Long.toString(creditCard));
-
-        TextView review_cardName = findViewById(R.id.review_cardName);
-        review_cardName.setText(cardName);
+//        TextView review_heading = findViewById(R.id.review_heading);
+//        review_heading.setText("Review Your Order");
+//
+//        TextView review_userName = findViewById(R.id.review_userName);
+//        review_userName.setText(userName);
+//
+//        TextView review_email = findViewById(R.id.review_email);
+//        review_email.setText(email);
+//
+//        TextView review_address = findViewById(R.id.review_address);
+//        review_address.setText(address);
+//
+//        TextView review_postalCode = findViewById(R.id.review_postalCode);
+//        review_postalCode.setText(postalCode);
+//
+//        TextView review_country = findViewById(R.id.review_country);
+//        review_country.setText(country);
+//
+//        TextView review_state = findViewById(R.id.review_state);
+//        review_state.setText(state);
+//
+//        TextView review_city = findViewById(R.id.review_city);
+//        review_city.setText(city);
+//
+//        TextView review_cardNo = findViewById(R.id.review_creditCard);
+//        review_cardNo.setText(Long.toString(creditCard));
+//
+//        TextView review_cardName = findViewById(R.id.review_cardName);
+//        review_cardName.setText(cardName);
 
 
 
