aeaa90f9befe8df3deab19c2dd2ea05b04dd4ac0
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2019-03-28 14:43:27 -0500

    Create UserBuilder.java

---
+++ app/src/main/java/comp3350/schrodingers/business/UserBuilder.java
@@ -0,0 +1,21 @@
+package comp3350.schrodingers.business;
+
+import comp3350.schrodingers.objects.User;
+import comp3350.schrodingers.objects.User.Address;
+import comp3350.schrodingers.objects.User.Billing;
+public class UserBuilder {
+    private User user;
+    public UserBuilder(int id, String email, String username, String password){
+        user = new User(id, email, username, password);
+    }
+
+    public User setAddress(Address address){
+        user = new User(user.getUserId(), user.getEmail(), user.getUserName(), user.getPassword(), address, user.getBilling());
+        return user;
+    }
+
+    public User setBilling(Billing billing){
+        user = new User(user.getUserId(), user.getEmail(), user.getUserName(), user.getPassword(), user.getAddress(), billing);
+        return user;
+    }
+}
