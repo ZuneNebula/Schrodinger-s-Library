c3feb414bcf6c2ab254f73b91cb824dbe1c1e2c3
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2019-04-05 00:41:52 -0500

    add paymentSys test

--- app/src/androidTest/java/comp3350/schrodingers/AccountTest.java
+++ app/src/androidTest/java/comp3350/schrodingers/AccountTest.java
@@ -1,6 +1,7 @@
 package comp3350.schrodingers;
 
 import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -26,6 +27,14 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.core.AllOf.allOf;
 
+import static android.support.test.espresso.contrib.DrawerActions.open;
+import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
+import static android.support.test.espresso.contrib.DrawerMatchers.isOpen;
+import static android.support.test.espresso.contrib.NavigationViewActions.navigateTo;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class AccountTest {
@@ -34,6 +43,51 @@ public class AccountTest {
 
     @Test
     public void createAccount(){
+        //logout from default user
+        onView(withId(R.id.my_account)).perform(click());
+        onView(withId(R.id.logout)).perform(click());
+
+        //create account
+        onView(withId(R.id.my_account)).perform(click());
+        onView(withId(R.id.account_button)).perform(click());
+        onView(withId(R.id.edtName)).perform(typeText("Chris"));
+        onView(withId(R.id.edtEmail)).perform(typeText("chris@gmail.com"));
+        onView(withId(R.id.edtPassword)).perform(typeText("comp3350"));
+        onView(withId(R.id.btnAccount)).perform(click());
+        //TODO: there seems to be a bug in create account logic
+
+        //verify account
+    }
+
+    @Test
+    public void addPaymentInformation(){
+        String cardNum = "1234123412341234";
+        String expiry = "05/25";
+        String cvv = "123";
+        String name = "Gordon Freeman";
+
+        // open options from main menu
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+                .perform(DrawerActions.open());
+        onView(withId(R.id.nav_view))
+                .perform(navigateTo(R.id.my_account));
+
+
+        onView(withId(R.id.paymentInfo)).perform(click());
+        //add payment info
+        onView(withId(R.id.billing)).perform(typeText(cardNum));
+        onView(withId(R.id.expDate)).perform(typeText(expiry));
+        onView(withId(R.id.cvv)).perform(typeText(cvv));
+        onView(withId(R.id.cardName)).perform(typeText(name));
+        onView(withId(R.id.resetPayment)).perform(click());
+
+        //verify that all information was added
+        onView(withId(R.id.paymentInfo)).perform(click());
+        onView(withId(R.id.billing)).check(matches(withText(cardNum)));
+        onView(withId(R.id.expDate)).check(matches(withText(expiry)));
+        onView(withId(R.id.cvv)).check(matches(withText(cvv)));
+        onView(withId(R.id.cardName)).check(matches(withText(name)));
 
     }
 }
